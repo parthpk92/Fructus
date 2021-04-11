81731e01e99cf3b89b3b466b1ad10a96780c0b87
Parth Kapadia <parth@bikayi.com>
2021-04-11 12:19:45 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Parth Kapadia on 11/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
